a80e5086064923a94c1180a7dd3eed713e671cb4
Arjun Nunugonda - a0n05ba <arjunchakri@gmail.com>
2025-10-25 21:11:06 +0530

    Update url-mappings.js

--- config/url-mappings.js
+++ config/url-mappings.js
@@ -5,34 +5,38 @@
 //   Query param: yoursite.com?key=<shortcut>  (fallback method)
 
 const urlMappings = {
+    // Notion web links
+    'dsa': 'https://arjunchakri.notion.site/Resources-18a85ef2a2298004b3f9d1156538e362',
+    'dsapractice': 'https://arjunchakri.notion.site/Preps-24c85ef2a229806088bbf0c4643f50c2',
+
     // Social Media
     'linkedin': 'https://www.linkedin.com/in/arjunchakravarthy/',
     'li': 'https://www.linkedin.com/in/arjunchakravarthy/',
-    
+
     // Resume/CV
     'resume': 'res/ArjunChakravarthyResume.pdf',
     'cv': 'res/ArjunChakravarthyResume.pdf',
-    
+
     // GitHub
     'github': 'https://github.com/arjunchakri',
     'gh': 'https://github.com/arjunchakri',
-    
+
     // Portfolio sections
     'portfolio': '#portfolio',
     'projects': '#projects',
     'contact': '#contact',
     'about': '#about',
-    
+
     // Project shortcuts (examples - adjust based on your actual projects)
     'notes': 'notes/index.html',
     'polls': 'polls/index.html',
     'chat': 'test-chat/index.html',
-    
+
     // External links (examples)
     'email': 'mailto:arjun@example.com',
     'twitter': 'https://twitter.com/arjunchakri',
     'instagram': 'https://instagram.com/arjunchakri',
-    
+
     // Utility shortcuts
     'home': 'index.html',
     'main': 'index.html'
